14f3258c9530eb2080a3c3619433580597be9b8a
renzhifan <zhifan6797@163.com>
2019-05-09 17:25:51 +0800

    11.组件通信（一）

---
+++ 11-main.js
@@ -0,0 +1,22 @@
+Vue.component('coupon',{
+    template:'<input placeholder="enter your coupon code" @blur="onCouponApplied">',
+
+    methods: {
+        onCouponApplied() {
+            this.$emit('applied');
+        }
+    }
+});
+
+new Vue({
+    el:'#root',
+    data: {
+        couponApplied:false
+    },
+
+    methods: {
+        onCouponApplied() {
+            this.couponApplied = true;
+        }
+    }
+});
